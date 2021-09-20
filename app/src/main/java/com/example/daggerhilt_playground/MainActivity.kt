9e07a2f290de6eeb631d13aa462ca0947a0ea313
mahdi <ssmmhh1382@gmail.com>
2021-09-20 18:09:42 +0430

    03- scoping with hilt

--- app/src/main/java/com/example/daggerhilt_playground/MainActivity.kt
+++ app/src/main/java/com/example/daggerhilt_playground/MainActivity.kt
@@ -3,28 +3,43 @@ package com.example.daggerhilt_playground
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
 import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.components.FragmentComponent
+import dagger.hilt.android.scopes.ActivityScoped
+import dagger.hilt.android.scopes.FragmentScoped
+import dagger.hilt.components.SingletonComponent
 import javax.inject.Inject
+import javax.inject.Singleton
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private val TAG = "MainActivity"
-//    field injection examp
+
+    //    field injection examp
     @Inject
     lateinit var cappuccinoMaker: CappuccinoMaker
+
+    @Inject
+    lateinit var cappuccinoMaker2: CappuccinoMaker
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.d(TAG, "onCreate: cappuccino: ${cappuccinoMaker.makeTheMilkHot()}")
-        Log.d(TAG, "onCreate: cappuccino: ${cappuccinoMaker.makeTheEspresso()}")
+        Log.d(TAG, "onCreate: cappuccino: ${cappuccinoMaker.hashCode()}")
+        Log.d(TAG, "onCreate: cappuccino: ${cappuccinoMaker2.hashCode()}")
     }
 }
+
 //field injection example
+//@Singleton
 class CappuccinoMaker
-@Inject
+//@Inject
 constructor(
     //constructor injection example
     private val milkHeater: MilkHeater
@@ -39,6 +54,7 @@ constructor(
     }
 
 }
+
 //constructor injection example
 class MilkHeater
 @Inject
@@ -48,4 +64,14 @@ constructor() {
         return "Milk is ready"
     }
 
+}
+
+@Module
+@InstallIn(ActivityComponent::class)
+object SomeModule {
+    @Provides
+    @ActivityScoped
+    fun provideCappuccinoMaker(milkHeater: MilkHeater): CappuccinoMaker {
+        return CappuccinoMaker(milkHeater)
+    }
 }
